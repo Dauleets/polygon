a18c8856445fa87ca00fb34a260f98d83f2ba2c2
dauleets <zhunussov.daulet.kz@gmail.com>
2023-07-12 10:55:33 +0600

    init: add project

---
+++ android/app/src/main/kotlin/com/example/polygontrade/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.polygontrade
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainWidget());
+}
+
+class MainWidget extends StatelessWidget {
+  const MainWidget({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Polygon Demo', home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
